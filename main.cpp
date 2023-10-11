1d0b8ba557f7b3ad90e2197250fcdf3c6b72b731
rowanV <verdouwr1827@my.uwstout.edu>
2023-10-11 18:56:43 -0500

    while loop and exit

--- main.cpp
+++ main.cpp
@@ -29,41 +29,52 @@ const string amogus
 
 int main()
 {
-	printf("Hello %s\n", "World!");
-	printf("Amogus\n");
-
-	int org_STDIN { dup(0) };
-	int org_STDOUT { dup(1) };
-
-	int fdChildToParent[2] {}; // [0] is reading
-	int fdParentToChild[2] {}; // [1] is writing
-
-	if (pipe(fdChildToParent) < 0)
+	while (true)
 	{
-		cout << "Failed to make fdChildToParent into a pipe" << '\n';
-	}
-	if (pipe(fdParentToChild) < 0)
-	{
-		cout << "Failed to make fdParentToChild into a pipe" << '\n';
-	}
+		printf("Hello %s\n", "World!");
+		printf("Amogus\n");
 
-	pid_t pid { fork() };
+		string command;
+		getline(cin, command);
 
-	if (pid < 0)
-	{
-		cout << "Failed to make a child." << '\n';
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		// Child process
+		if (command == "exit")
+		{
+			break;
+		}
 
-		exit(0);
-	}
-	else
-	{
-		// Parent process
-		cout << amogus;
+		int org_STDIN { dup(0) };
+		int org_STDOUT { dup(1) };
+
+		int fdChildToParent[2] {}; // [0] is reading
+		int fdParentToChild[2] {}; // [1] is writing
+
+		if (pipe(fdChildToParent) < 0)
+		{
+			cout << "Failed to make fdChildToParent into a pipe" << '\n';
+		}
+		if (pipe(fdParentToChild) < 0)
+		{
+			cout << "Failed to make fdParentToChild into a pipe" << '\n';
+		}
+
+		pid_t pid { fork() };
+
+		if (pid < 0)
+		{
+			cout << "Failed to make a child." << '\n';
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			// Child process
+
+			exit(0);
+		}
+		else
+		{
+			// Parent process
+			cout << amogus;
+		}
 	}
 
 	return 0;
